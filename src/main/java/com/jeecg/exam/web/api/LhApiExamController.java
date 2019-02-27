887d0747b4f3662e99787cb97d2d9bf011597b2b
zhangqg123 <growingway@qq.com>
2019-02-27 10:17:20 +0800

    student

--- src/main/java/com/jeecg/exam/web/api/LhApiExamController.java
+++ src/main/java/com/jeecg/exam/web/api/LhApiExamController.java
@@ -113,6 +113,24 @@ public class LhApiExamController extends BaseController{
 		j.setObj(list.getResults());
 		return j;
 	}
+	@RequestMapping(value="/userPhone")
+	public @ResponseBody AjaxJson userPhone(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		AjaxJson j = new AjaxJson();
+		j.setSuccess(false);
+		String phone = request.getParameter("phone");
+		String xcxId = request.getParameter("xcxId");
+		if(phone!=null&&xcxId!=null){
+			LhSAccountEntity lhSAccount=new LhSAccountEntity();
+			lhSAccount.setPhone(phone);
+			lhSAccount.setAppId(xcxId);
+			MiniDaoPage<LhSAccountEntity> list = lhSAccountService.getAll(lhSAccount, 1, 100);
+			if(list.getResults()!=null&&list.getResults().size()>0){
+				j.setSuccess(true);
+				j.setObj(list.getResults().get(0));
+			}
+		}
+		return j;
+	}
 	
 	@RequestMapping(value= "/login", method = RequestMethod.POST)
 	public @ResponseBody AjaxJson login(HttpServletRequest request, @RequestBody LhSUserEntity lhSUser) {
